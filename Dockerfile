30b85946f7527f8499c9f5e97fe1f9a2787fd47a
Himeshi <thleaderh@gmail.com>
2026-07-11 14:32:49 +0900

    enable plantUML

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,9 @@
 FROM vvakame/review:5.9
 
+RUN apt-get update -qq \
+    && apt-get install -y -qq --no-install-recommends graphviz \
+    && rm -rf /var/lib/apt/lists/*
+
 WORKDIR /book
 
 COPY . /book
--- build-in-docker.sh
+++ build-in-docker.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
-[ ! -z $REVIEW_CONFIG_FILE ] || REVIEW_CONFIG_FILE=config.yml
+set -eu
+
+REVIEW_CONFIG_FILE=${REVIEW_CONFIG_FILE:-config.yml}
+REVIEW_DOCKER_IMAGE=${REVIEW_DOCKER_IMAGE:-team-d-kuroudo-engineer-review:5.9}
 
 # コマンド手打ちで作業したい時は以下の通り /book に pwd がマウントされます
-# docker run -i -t -v $(pwd):/book vvakame/review:5.9 /bin/bash
+# docker run -i -t -v "$(pwd):/book" team-d-kuroudo-engineer-review:5.9 /bin/bash
 
-docker run -t --rm -v $(pwd):/book vvakame/review:5.9 /bin/bash -ci "cd /book && ./setup.sh && REVIEW_CONFIG_FILE=$REVIEW_CONFIG_FILE npm run pdf"
+docker build -t "$REVIEW_DOCKER_IMAGE" .
+docker run -t --rm \
+  -v "$(pwd):/book" \
+  -e REVIEW_CONFIG_FILE="$REVIEW_CONFIG_FILE" \
+  "$REVIEW_DOCKER_IMAGE" \
+  /bin/bash -ci 'cd /book && ./setup.sh && npm run pdf'
